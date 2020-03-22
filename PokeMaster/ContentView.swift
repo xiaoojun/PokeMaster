66a9ad0c6a8ff12cf57fc40a875f05907d281232
xiaojun <961721716@qq.com>
2020-03-22 19:05:45 +0800

    Initial Commit

---
+++ PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by 汤小军 on 2020/3/22.
+//  Copyright © 2020 汤小军. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
